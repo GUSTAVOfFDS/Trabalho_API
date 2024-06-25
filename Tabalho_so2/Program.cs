a13901b6dc89c8ea3ce8634819e3a3de2b731a3e
gustavo <gusf_freitas@hotmail.com>
2024-06-25 18:00:16 -0300

    Scaffolding a WebApi Projetc

--- Tabalho_so2/Program.cs
+++ Tabalho_so2/Program.cs
@@ -1,8 +1,15 @@
+using Trabalho_so2.Services.Implementations;
+using Trabalho_so2.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+ 
+
 
 var app = builder.Build();
 
